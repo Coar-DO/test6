fd92329e39b683ce0d9794d02aaa2cf47075cb81
Coar-DO <143054357+Coar-DO@users.noreply.github.com>
2023-12-01 17:52:36 +0900

    Add files via upload

--- app/app.js
+++ app/app.js
@@ -1,25 +1,48 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
-// Express 애플리케이션 설정
 const app = express();
 const port = 3000;
+
+// MongoDB 연결 설정
+mongoose.connect("mongodb://localhost:27017/webexample", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// MongoDB 스키마 정의
+const numberSchema = new mongoose.Schema({
+  value: Number,
+});
+
+// MongoDB 모델 정의
+const NumberModel = mongoose.model("Number", numberSchema);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// 라우트 정의
 app.get("/", (req, res) => {
   res.send(
     'Web Example: Enter a number <form method="post" action="/save"><input type="number" name="value" required><button type="submit">Save</button></form>'
   );
 });
 
-app.post("/save", (req, res) => {
+app.post("/save", async (req, res) => {
   const { value } = req.body;
+
+  // MongoDB에 저장
+  await NumberModel.create({ value });
+
   res.send(`You entered: ${value}`);
 });
 
-// 서버 시작
+app.get("/show", async (req, res) => {
+  // MongoDB에서 모든 문서 조회
+  const numbers = await NumberModel.find();
+
+  res.send(`Stored Numbers: ${numbers.map((num) => num.value).join(", ")}`);
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
